9cb71b05c7b93b31f7a1e7f8a576fedf2029a53c
olaf-snow <olaf.snow@navercorp.com>
2021-03-07 11:58:18 +0900

    webtoon maker 추가

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById('app'))
+import App from './app'
+
+ReactDOM.render(<App />, document.getElementById('app'))
