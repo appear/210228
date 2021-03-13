67ba6edb5118507a6836debd4af0a4c9d2d39d07
olaf-snow <olaf.snow@navercorp.com>
2021-03-13 22:20:39 +0900

    feature: reset css 추가

--- src/app.js
+++ src/app.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import styled from 'styled-components'
 
 import { ROUTES } from './constants'
 
 import WebtoonPage from './pages/webtoon'
 import MainPage from './pages/main'
+import ResetCSS from './components/reset-css'
+
+const Container = styled.div`
+  max-width: 720px;
+  height: 100vh;
+`
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact>
-          <MainPage />
-        </Route>
-        <Route path={ROUTES.WEBTOON.MAIN}>
-          <WebtoonPage />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Container>
+      <ResetCSS />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <MainPage />
+          </Route>
+          <Route path={ROUTES.WEBTOON.MAIN}>
+            <WebtoonPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </Container>
   )
 }
 
