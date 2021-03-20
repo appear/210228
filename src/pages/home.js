2613e6ecc77e951c76e23df746332722b6994a38
olaf-snow <olaf.snow@navercorp.com>
2021-03-20 21:22:07 +0900

    home link 추가

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import moment from 'moment'
+
+import { ROUTES } from '../constants'
+
+const DAY_OF_WEEK = ['일', '월', '화', '수', '목', '금', '토']
 
 function HomePage() {
-  return <div>HomePage</div>
+  const weekdayLabel = `${DAY_OF_WEEK[moment().day()]}요웹툰`
+
+  return (
+    <div>
+      <Link to={ROUTES.WEBTOON.HOME}>웹툰홈</Link> |{' '}
+      <Link to={ROUTES.WEBTOON.WEEKDAY}>{weekdayLabel}</Link>
+    </div>
+  )
 }
 
 export default HomePage
